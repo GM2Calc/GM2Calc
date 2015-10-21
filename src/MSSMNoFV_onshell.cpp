34c8024b3a057a8976972abdbb03e4b44f27cbd8
Alexander Voigt <Alexander.Voigt@desy.de>
2015-10-21 15:14:49 +0200

    extend comment

--- src/MSSMNoFV_onshell.cpp
+++ src/MSSMNoFV_onshell.cpp
@@ -137,7 +137,7 @@ void MSSMNoFV_onshell::convert_to_onshell(
    convert_yukawa_couplings(); // first guess of resummed yukawas
    convert_ml2();
    convert_yukawa_couplings();
-   calculate_MSm(); // calculate MSm with new value of ml2(1,1)
+   calculate_MSm(); // calculate MSm with new value of ml2(1,1) and ym
    convert_me2(precision, max_iterations);
    convert_yukawa_couplings();
 
