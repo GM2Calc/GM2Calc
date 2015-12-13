70ab113793d3d870d5ef3c07ba00ae97e8198c34
Alexander Voigt <Alexander.Voigt@desy.de>
2015-12-13 10:56:28 +0100

    adding CHECK_CLOSE_ABS and CHECK_CLOSE_REL test macros
    to check absolute and relative equality up to a certain offset

--- test/test.hpp
+++ test/test.hpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
+#include <limits>
 
 namespace gm2calc_test {
 
@@ -19,6 +21,42 @@ static bool g_verbose = false;
 #define CHECK_EQUAL(a, b) \
    check_equal((a), (b), #a, #b)
 
+#define CHECK_CLOSE_ABS(a, b, eps) \
+   check_close_abs((a), (b), (eps), #a, #b)
+
+#define CHECK_CLOSE_REL(a, b, eps) \
+   check_close_rel((a), (b), (eps), #a, #b)
+
+template <typename T>
+bool is_zero(T a, T prec = std::numeric_limits<T>::epsilon())
+{
+   return std::abs(a) < prec;
+}
+
+template <typename T>
+bool is_equal(T a, T b, T prec = std::numeric_limits<T>::epsilon())
+{
+   return is_zero(a - b, prec);
+}
+
+template <typename T>
+bool is_equal(long a, long b, T prec = std::numeric_limits<T>::epsilon())
+{
+   return is_zero(a - b, prec);
+}
+
+template <typename T>
+bool is_equal_rel(T a, T b, T prec = std::numeric_limits<T>::epsilon())
+{
+   if (is_equal(a, b, std::numeric_limits<T>::epsilon()))
+      return true;
+
+   if (std::abs(a) < std::numeric_limits<T>::epsilon())
+      return is_equal(a, b, prec);
+
+   return std::abs((a - b)/a) < prec;
+}
+
 void check_equal(double a, double b, const std::string& astr, const std::string& bstr)
 {
    if (a == b) {
@@ -27,6 +65,36 @@ void check_equal(double a, double b, const std::string& astr, const std::string&
    } else {
       std::cout << "test failed: " << astr << " == " << bstr
                 << ": " << a << " != " << b << std::endl;
+      g_error++;
+   }
+}
+
+void check_close_abs(double a, double b, double eps, const std::string& astr, const std::string& bstr)
+{
+   if (is_equal(a, b, eps)) {
+      VERBOSE_MSG("test passed: " << astr << " ~ " << bstr
+                  << ": " << a << " ~ " << b
+                  << " (required maximum absolute difference: " << eps << ")");
+   } else {
+      std::cout << "test failed: " << astr << " ~ " << bstr
+                << ": " << a << " !~ " << b
+                << " (required maximum absolute difference: " << eps << ")"
+                << std::endl;
+      g_error++;
+   }
+}
+
+void check_close_rel(double a, double b, double eps, const std::string& astr, const std::string& bstr)
+{
+   if (is_equal_rel(a, b, eps)) {
+      VERBOSE_MSG("test passed: " << astr << " ~ " << bstr
+                  << ": " << a << " ~ " << b
+                  << " (required maximum relative difference: " << eps);
+   } else {
+      std::cout << "test failed: " << astr << " ~ " << bstr
+                << ": " << a << " !~ " << b
+                << " (required maximum relative difference: " << eps << ")"
+                << std::endl;
       g_error++;
    }
 }
