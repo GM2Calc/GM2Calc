465177943df256e4aa6cae7bd020b1a8685d0643
Alexander Voigt <ahgvoigt@gmail.com>
2020-01-21 21:43:42 +0100

    test mono-variate functions

--- test/test_ffunctions.cpp
+++ test/test_ffunctions.cpp
@@ -2,6 +2,7 @@
 
 #include "doctest.h"
 #include "ffunctions.hpp"
+#include "read_data.hpp"
 #include <limits>
 
 #define CHECK_CLOSE(a,b,eps)                            \
@@ -68,4 +69,45 @@ TEST_CASE("symmetries")
    CHECK_CLOSE(Iabc(1.0, 2.0, 3.0), Iabc(2.0, 1.0, 3.0), eps);
    CHECK_CLOSE(Iabc(1.0, 2.0, 3.0), Iabc(1.0, 3.0, 2.0), eps);
    CHECK_CLOSE(Iabc(1.0, 2.0, 3.0), Iabc(3.0, 2.0, 1.0), eps);
+}
+
+template <typename T>
+void test_1(const char* func_name, T func, double eps)
+{
+   const std::string filename(std::string(TEST_DATA_DIR) + PATH_SEPARATOR + func_name + ".txt");
+   const auto data = gm2calc::test::read_from_file<double>(filename);
+
+   for (const auto v: data) {
+      if (v.size() < 2) {
+         continue;
+      }
+
+      const auto x          = v.at(0);
+      const auto f_expected = v.at(1);
+      const auto f_gm2calc  = func(x);
+
+      INFO("x = " << x << ", " << func_name << "(expected) = " << f_expected
+                  << ", " << func_name << "(gm2calc) = " << f_gm2calc);
+
+      CHECK_CLOSE(f_expected, f_gm2calc, eps);
+   }
+}
+
+TEST_CASE("test_F")
+{
+   test_1("F1C", [] (double x) { return gm2calc::F1C(x); }, 1e-9);
+   test_1("F2C", [] (double x) { return gm2calc::F2C(x); }, 1e-9);
+   test_1("F3C", [] (double x) { return gm2calc::F3C(x); }, 1e-9);
+   test_1("F4C", [] (double x) { return gm2calc::F4C(x); }, 1e-9);
+
+   test_1("F1N", [] (double x) { return gm2calc::F1N(x); }, 1e-9);
+   test_1("F2N", [] (double x) { return gm2calc::F2N(x); }, 1e-9);
+   test_1("F3N", [] (double x) { return gm2calc::F3N(x); }, 1e-9);
+   test_1("F4N", [] (double x) { return gm2calc::F4N(x); }, 1e-9);
+}
+
+TEST_CASE("test_G")
+{
+   test_1("G3", [] (double x) { return gm2calc::G3(x); }, 1e-9);
+   test_1("G4", [] (double x) { return gm2calc::G4(x); }, 1e-9);
 }
