445ea1c0cbea37023ceb7863ee6c6e63ee9c9818
Alexander Voigt <ahgvoigt@gmail.com>
2020-03-05 19:21:19 +0100

    test invalid GM2CalcConfig input

--- test/test_slha_io.cpp
+++ test/test_slha_io.cpp
@@ -12,7 +12,7 @@
    } while (0)
 
 
-TEST_CASE("fill_config")
+TEST_CASE("fill_valid_config")
 {
    gm2calc::Config_options config;
 
@@ -35,6 +35,35 @@ Block GM2CalcConfig
    CHECK(config.output_format == 1);
    CHECK(config.loop_order == 1);
    CHECK(config.tanb_resummation == false);
+   CHECK(config.force_output == true);
+   CHECK(config.verbose_output == true);
+   CHECK(config.calculate_uncertainty == true);
+}
+
+TEST_CASE("fill_invalid_config")
+{
+   const gm2calc::Config_options default_config;
+   gm2calc::Config_options config;
+
+   char const * const slha_input = R"(
+Block GM2CalcConfig
+     0     9     # output format (0, 1, 2, 3, 4)
+     1     9     # loop order (0, 1 or 2)
+     2     2.5   # disable/enable tan(beta) resummation (0 or 1)
+     3     2.5   # force output (0 or 1)
+     4     2.5   # verbose output (0 or 1)
+     5     2.5   # calculate uncertainty
+)";
+
+   std::istringstream stream(slha_input);
+
+   gm2calc::GM2_slha_io slha;
+   slha.read_from_stream(stream);
+   slha.fill(config);
+
+   CHECK(config.output_format == default_config.output_format);
+   CHECK(config.loop_order == default_config.loop_order);
+   CHECK(config.tanb_resummation == true);
    CHECK(config.force_output == true);
    CHECK(config.verbose_output == true);
    CHECK(config.calculate_uncertainty == true);
