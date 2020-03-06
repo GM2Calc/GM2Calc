737bba9c35006da2ac9a290ff60a76892faff580
Alexander Voigt <ahgvoigt@gmail.com>
2020-03-06 11:15:51 +0100

    reading gluino mass for completeness

--- src/gm2_slha_io.cpp
+++ src/gm2_slha_io.cpp
@@ -664,7 +664,7 @@ void process_mass_tuple(
    case 35     : physical.Mhh(1) = value;   break;
    case 36     : physical.MAh(1) = value;   break;
    case 37     : physical.MHpm(1) = value;  break;
-   case 1000021: /* gluino */               break;
+   case 1000021: physical.MGlu = value;     break;
    case 1000022: physical.MChi(0) = value;  break;
    case 1000023: physical.MChi(1) = value;  break;
    case 1000025: physical.MChi(2) = value;  break;
