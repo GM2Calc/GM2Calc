8a4d68090d25a7fb057741b5602d1a7f167faed0
Alexander Voigt <ahgvoigt@gmail.com>
2020-03-12 19:39:20 +0100

    test SM pole masses

--- test/test_MSSMNoFV_onshell.cpp
+++ test/test_MSSMNoFV_onshell.cpp
@@ -137,4 +137,11 @@ TEST_CASE("conversion_to_onshell")
    CHECK_CLOSE(model.get_MCha(0), model.get_physical().MCha(0), eps);
    CHECK_CLOSE(model.get_MCha(1), model.get_physical().MCha(1), eps);
    CHECK_CLOSE(model.get_MA0()  , model.get_physical().MAh(1) , eps);
+
+   CHECK_CLOSE(model.get_MU()   , model.get_physical().MFu    , eps);
+   CHECK_CLOSE(model.get_MD()   , model.get_physical().MFd    , eps);
+   CHECK_CLOSE(model.get_MC()   , model.get_physical().MFc    , eps);
+   CHECK_CLOSE(model.get_MS()   , model.get_physical().MFs    , eps);
+   CHECK_CLOSE(model.get_MBMB() , model.get_physical().MFb    , eps);
+   CHECK_CLOSE(model.get_MT()   , model.get_physical().MFt    , eps);
 }
