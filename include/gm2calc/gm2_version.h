7030d73b12ce66ad472143ec43a410b6cd6e5426
Alexander Voigt <ahgvoigt@gmail.com>
2020-04-26 13:28:37 +0200

    version 1.7.0

--- include/gm2calc/gm2_version.h
+++ include/gm2calc/gm2_version.h
@@ -21,8 +21,8 @@
 
 /* GM2Calc version */
 #define GM2CALC_VERSION_MAJOR 1
-#define GM2CALC_VERSION_MINOR 6
+#define GM2CALC_VERSION_MINOR 7
 #define GM2CALC_VERSION_PATCH 0
-#define GM2CALC_VERSION "1.6.0"
+#define GM2CALC_VERSION "1.7.0"
 
 #endif
