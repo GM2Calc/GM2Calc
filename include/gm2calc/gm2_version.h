205d61af627173e0f11e5bc73319aeff6cf444db
Alexander Voigt <ahgvoigt@gmail.com>
2021-01-10 00:36:57 +0100

    version 1.7.2

--- include/gm2calc/gm2_version.h
+++ include/gm2calc/gm2_version.h
@@ -22,7 +22,7 @@
 /* GM2Calc version */
 #define GM2CALC_VERSION_MAJOR 1
 #define GM2CALC_VERSION_MINOR 7
-#define GM2CALC_VERSION_PATCH 1
-#define GM2CALC_VERSION "1.7.1"
+#define GM2CALC_VERSION_PATCH 2
+#define GM2CALC_VERSION "1.7.2"
 
 #endif
