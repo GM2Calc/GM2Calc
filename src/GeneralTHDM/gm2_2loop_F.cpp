bc99aeceb73816dde0f9891ea3364c77acd2fd7b
Alexander Voigt <ahgvoigt@gmail.com>
2021-06-21 13:03:20 +0200

    correcting comment

--- src/GeneralTHDM/gm2_2loop_F.cpp
+++ src/GeneralTHDM/gm2_2loop_F.cpp
@@ -50,7 +50,7 @@ struct F_neut_pars {
 const double pi = 3.1415926535897932;
 const double q_u =  2.0/3.0;  ///< electric charge of up-type quarks
 const double q_d = -1.0/3.0;  ///< electric charge of down-type quarks
-const double q_l = -1.0;      ///< electric charge of down-type quarks
+const double q_l = -1.0;      ///< electric charge of down-type leptons
 const double t3_u = +1.0;     ///< SU(2)_L charge of up-type quark @todo(alex) check convention
 const double t3_d = -1.0;     ///< SU(2)_L charge of down-type quark @todo(alex) check convention
 const double t3_l = -1.0;     ///< SU(2)_L charge of charged lepton @todo(alex) check convention
