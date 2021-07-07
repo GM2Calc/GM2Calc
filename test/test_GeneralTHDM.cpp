6a38aad5c4255b8b522582abf2162806557ec055
Alexander Voigt <ahgvoigt@gmail.com>
2021-07-07 09:18:40 +0200

    remove unused header

--- test/test_GeneralTHDM.cpp
+++ test/test_GeneralTHDM.cpp
@@ -4,7 +4,6 @@
 #include "gm2calc/GeneralTHDM.hpp"
 
 #include <cmath>
-#include <iostream>
 
 #define CHECK_CLOSE(a,b,eps)                            \
    do {                                                 \
