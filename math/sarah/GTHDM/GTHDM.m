11a91a96bac363baea93503b1a08b927efeb55c4
Alexander Voigt <ahgvoigt@gmail.com>
2021-07-07 14:38:24 +0200

    code formatting

--- math/sarah/GTHDM/GTHDM.m
+++ math/sarah/GTHDM/GTHDM.m
@@ -22,10 +22,10 @@
 
 Off[General::spell]
 
-Model`Name = "THDM";
+Model`Name = "GTHDM";
 Model`NameLaTeX ="General Two Higgs Doublet Model";
-Model`Authors = "F.Staub,A.Voigt";
-Model`Date = "2015-11-16";
+Model`Authors = "D.Jacob,F.Staub,A.Voigt";
+Model`Date = "2021-07-07";
 
 (*-------------------------------------------*)
 (*   Particle Content*)
@@ -33,25 +33,21 @@ Model`Date = "2015-11-16";
 
 (* Gauge Superfields *)
 
-Gauge[[1]]={B,   U[1], hypercharge, g1,False};
-Gauge[[2]]={WB, SU[2], left,        g2,True};
-Gauge[[3]]={G,  SU[3], color,       g3,False};
+Gauge[[1]]={B,   U[1], hypercharge, g1, False};
+Gauge[[2]]={WB, SU[2], left,        g2, True };
+Gauge[[3]]={G,  SU[3], color,       g3, False};
 
 
 (* Chiral Superfields *)
 
-FermionFields[[1]] = {q, 3, {uL,dL},     1/6, 2, 3};  
-FermionFields[[2]] = {l, 3, {vL,eL},    -1/2, 2, 1};
-FermionFields[[3]] = {d, 3, conj[dR],   1/3, 1, -3};
-FermionFields[[4]] = {u, 3, conj[uR],  -2/3, 1, -3};
-FermionFields[[5]] = {e, 3, conj[eR],     1, 1,  1};
+FermionFields[[1]] = {q, 3, {uL,dL},     1/6, 2,  3};
+FermionFields[[2]] = {l, 3, {vL,eL},    -1/2, 2,  1};
+FermionFields[[3]] = {d, 3, conj[dR],    1/3, 1, -3};
+FermionFields[[4]] = {u, 3, conj[uR],   -2/3, 1, -3};
+FermionFields[[5]] = {e, 3, conj[eR],      1, 1,  1};
 
-ScalarFields[[1]] =  {H1, 1, {H1p, H10},     1/2, 2,  1};
-ScalarFields[[2]] =  {H2, 1, {H2p, H20},     1/2, 2,  1};
-
-
-     
-
+ScalarFields[[1]]  = {H1, 1, {H1p, H10}, 1/2, 2,  1};
+ScalarFields[[2]]  = {H2, 1, {H2p, H20}, 1/2, 2,  1};
 
 (*----------------------------------------------*)
 (*   DEFINITION                                 *)
@@ -66,10 +62,12 @@ DEFINITION[GaugeES][Additional]= {
 	{LagNoHC,{ AddHC->False}}
 };
 
-
-LagNoHC = -(M112 conj[H1].H1 + M222 conj[H2].H2 + Lambda1/2 conj[H1].H1.conj[H1].H1 + \
-		Lambda2/2 conj[H2].H2.conj[H2].H2 + Lambda3 conj[H2].H2.conj[H1].H1 + Lambda4 conj[H2].H1.conj[H1].H2 );
-
+LagNoHC = -(+ M112 conj[H1].H1
+            + M222 conj[H2].H2
+            + Lambda1/2 conj[H1].H1.conj[H1].H1
+            + Lambda2/2 conj[H2].H2.conj[H2].H2
+            + Lambda3 conj[H2].H2.conj[H1].H1
+            + Lambda4 conj[H2].H1.conj[H1].H2);
 
 LagHC = -(-M122 conj[H1].H2
           + Lambda5/2 conj[H2].H1.conj[H2].H1
@@ -80,48 +78,44 @@ LagHC = -(-M122 conj[H1].H2
 
 (* Gauge Sector *)
 
-DEFINITION[EWSB][GaugeSector] =
-{ 
-  {{VB,VWB[3]},{VP,VZ},ZZ},
-  {{VWB[1],VWB[2]},{VWm,conj[VWm]},ZW}
-};     
-        
-        
-          	
+DEFINITION[EWSB][GaugeSector] = {
+    {{VB,VWB[3]},{VP,VZ},ZZ},
+    {{VWB[1],VWB[2]},{VWm,conj[VWm]},ZW}
+};
 
 (* ----- VEVs ---- *)
 
-DEFINITION[EWSB][VEVs]= 
-{    {H10, {v1, 1/Sqrt[2]}, {sigma1, \[ImaginaryI]/Sqrt[2]},{phi1, 1/Sqrt[2]}},
-     {H20, {v2, 1/Sqrt[2]}, {sigma2, \[ImaginaryI]/Sqrt[2]},{phi2, 1/Sqrt[2]}}     };
- 
+DEFINITION[EWSB][VEVs] = {
+    {H10, {v1, 1/Sqrt[2]}, {sigma1, \[ImaginaryI]/Sqrt[2]},{phi1, 1/Sqrt[2]}},
+    {H20, {v2, 1/Sqrt[2]}, {sigma2, \[ImaginaryI]/Sqrt[2]},{phi2, 1/Sqrt[2]}}
+};
 
-DEFINITION[EWSB][MatterSector]=   
-    { {{phi1, phi2}, {hh, ZH}},
-      {{sigma1, sigma2}, {Ah, ZA}},
-      {{conj[H1p],conj[H2p]},{Hm,ZP}},
-      {{{dL}, {conj[dR]}}, {{DL,Vd}, {DR,Ud}}},
-      {{{uL}, {conj[uR]}}, {{UL,Vu}, {UR,Uu}}},
-      {{{eL}, {conj[eR]}}, {{EL,Ve}, {ER,Ue}}}       };  
 
+DEFINITION[EWSB][MatterSector] = {
+    {{phi1, phi2}, {hh, ZH}},
+    {{sigma1, sigma2}, {Ah, ZA}},
+    {{conj[H1p],conj[H2p]},{Hm,ZP}},
+    {{{dL}, {conj[dR]}}, {{DL,Vd}, {DR,Ud}}},
+    {{{uL}, {conj[uR]}}, {{UL,Vu}, {UR,Uu}}},
+    {{{eL}, {conj[eR]}}, {{EL,Ve}, {ER,Ue}}}
+};
 
 (*------------------------------------------------------*)
 (* Dirac-Spinors *)
 (*------------------------------------------------------*)
 
-DEFINITION[EWSB][DiracSpinors]={
- Fd ->{  DL, conj[DR]},
- Fe ->{  EL, conj[ER]},
- Fu ->{  UL, conj[UR]},
- Fv ->{  vL, 0}};
+DEFINITION[EWSB][DiracSpinors] = {
+    Fd ->{DL, conj[DR]},
+    Fe ->{EL, conj[ER]},
+    Fu ->{UL, conj[UR]},
+    Fv ->{vL, 0}
+};
 
-DEFINITION[EWSB][GaugeES]={
- Fd1 ->{  FdL, 0},
- Fd2 ->{  0, FdR},
- Fu1 ->{  Fu1, 0},
- Fu2 ->{  0, Fu2},
- Fe1 ->{  Fe1, 0},
- Fe2 ->{  0, Fe2}};
-
-
-
+DEFINITION[EWSB][GaugeES] = {
+    Fd1 ->{FdL, 0},
+    Fd2 ->{0, FdR},
+    Fu1 ->{Fu1, 0},
+    Fu2 ->{0, Fu2},
+    Fe1 ->{Fe1, 0},
+    Fe2 ->{0, Fe2}
+};
