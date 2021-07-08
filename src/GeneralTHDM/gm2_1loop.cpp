d7520ce84ed9ba9bc4e2aa073779391b4eddf122
Alexander Voigt <ahgvoigt@gmail.com>
2021-07-08 11:13:07 +0200

    match model parameters to 1-loop amu parameters

--- src/GeneralTHDM/gm2_1loop.cpp
+++ src/GeneralTHDM/gm2_1loop.cpp
@@ -19,6 +19,7 @@
 #include "gm2calc/gm2_1loop.hpp"
 #include "gm2calc/GeneralTHDM.hpp"
 #include "GeneralTHDM/gm2_1loop_helpers.hpp"
+#include <cmath>
 
 /**
  * \file gm2_1loop.cpp
@@ -29,16 +30,37 @@
 
 namespace gm2calc {
 
+namespace {
+
+const double sqrt2 = 1.4142135623730950; // Sqrt[2]
+double sqr(double x) noexcept { return x*x; }
+
+} // anonymous namespace
+
 /**
  * Calculates full 1-loop contribution to a_mu in the general THDM.
  *
  * @todo(alex) to be implemented
+ * @todo(alex) check convention for ylHp
  *
  * @param model THDM model parameters, masses and mixings
  * @return 1-loop contribution to a_mu
  */
 double calculate_amu_1loop(const GeneralTHDM& model)
 {
+   const double alpha_h = model.get_alpha_h();
+   const double beta = model.get_beta();
+   const double tb = model.get_tan_beta();
+   const double cb = 1./std::sqrt(1.0 + sqr(tb));
+   const double sba = std::sin(beta - alpha_h);
+   const double cba = std::cos(beta - alpha_h);
+   const double v = model.get_v();
+   const double mm = model.get_MFe(1);
+   const Eigen::Matrix<double,3,3> id = Eigen::Matrix<double,3,3>::Identity();
+
+   const Eigen::Matrix<double,3,3> zetal =
+      model.get_Xe()*v/(cb*sqrt2*mm) - tb*id;
+
    general_thdm::THDM_1L_parameters pars;
    pars.alpha_em = model.get_alpha_em();
    pars.mm = model.get_MFe(1);
@@ -50,10 +72,10 @@ double calculate_amu_1loop(const GeneralTHDM& model)
    pars.ml = model.get_MFe();
    pars.mv = model.get_MFv();
    pars.mh = model.get_Mhh();
-   // pars.ylh = ?;
-   // pars.ylH = ?;
-   // pars.ylA = ?;
-   // pars.ylHp = ?;
+   pars.ylh = sba*id + cba*zetal; // Eq.(18), arxiv:1607.06292
+   pars.ylH = cba*id - sba*zetal; // Eq.(18), arxiv:1607.06292
+   pars.ylA = -zetal;             // Eq.(18), arxiv:1607.06292
+   pars.ylHp = pars.ylA; // @todo(alex) check convention
 
    return general_thdm::amu1L(pars);
 }
