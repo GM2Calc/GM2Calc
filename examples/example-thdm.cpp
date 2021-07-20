741deaa762f0c52d7a7f9f83b510016e02c1cd9d
Alexander Voigt <ahgvoigt@gmail.com>
2021-07-20 12:23:41 +0200

    adding C++ example THDM

---
+++ examples/example-thdm.cpp
@@ -0,0 +1,44 @@
+#include "gm2calc/gm2_1loop.hpp"
+#include "gm2calc/gm2_2loop.hpp"
+#include "gm2calc/gm2_uncertainty.hpp"
+#include "gm2calc/gm2_error.hpp"
+#include "gm2calc/GeneralTHDM.hpp"
+
+#include <iostream>
+
+int main()
+{
+   gm2calc::GeneralTHDM::Physical_basis basis;
+   basis.mh = 125;
+   basis.mH = 400;
+   basis.mA = 420;
+   basis.mHp = 440;
+   basis.sin_beta_minus_alpha = 0.999;
+   basis.lambda6 = 0;
+   basis.lambda7 = 0;
+   basis.tan_beta = 3;
+   basis.m122 = 40000;
+
+   gm2calc::SM sm;
+   sm.set_alpha_em_mz(1.0/127.934);
+   sm.set_mu(2, 172.5);
+   sm.set_mu(1, 1.42);
+   sm.set_md(2, 4.75);
+   sm.set_ml(2, 1.77684);
+
+   try {
+      gm2calc::GeneralTHDM model(basis);
+
+      const double amu = gm2calc::calculate_amu_1loop(model)
+                       + gm2calc::calculate_amu_2loop(model);
+
+      const double delta_amu =
+         gm2calc::calculate_uncertainty_amu_2loop(model);
+
+      std::cout << "amu = " << amu << " +- " << delta_amu << '\n';
+   } catch (const gm2calc::Error& e) {
+      std::cout << e.what() << '\n';
+   }
+
+   return 0;
+}
