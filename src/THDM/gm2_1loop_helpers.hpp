2204b795783ab631ac4d2aee18c8fef1f60d08fb
Alexander Voigt <ahgvoigt@gmail.com>
2021-08-02 16:52:52 +0200

    shorten header include guard

--- src/THDM/gm2_1loop_helpers.hpp
+++ src/THDM/gm2_1loop_helpers.hpp
@@ -16,8 +16,8 @@
 // <http://www.gnu.org/licenses/>.
 // ====================================================================
 
-#ifndef GM2_GENERALTHDM_1LOOP_HELPERS_HPP
-#define GM2_GENERALTHDM_1LOOP_HELPERS_HPP
+#ifndef GM2_THDM_1LOOP_HELPERS_HPP
+#define GM2_THDM_1LOOP_HELPERS_HPP
 
 #include <Eigen/Core>
 
