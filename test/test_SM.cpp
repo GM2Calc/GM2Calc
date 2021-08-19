e2829f559bccff9db9e4702131d9a6ce5f540fe7
Alexander Voigt <ahgvoigt@gmail.com>
2021-08-19 10:13:26 +0200

    adding test for CKM matrix unitarity

---
+++ test/test_SM.cpp
@@ -0,0 +1,18 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN 1
+
+#include "doctest.h"
+#include "gm2calc/SM.hpp"
+#include <limits>
+
+// check unitarity of default CKM matrix
+TEST_CASE("test-CKM-unitarity")
+{
+   const double eps = std::numeric_limits<double>::epsilon();
+
+   const Eigen::Matrix<std::complex<double>,3,3> ckm = gm2calc::SM().get_ckm();
+   const Eigen::Matrix<std::complex<double>,3,3> unit = Eigen::Matrix<std::complex<double>,3,3>::Identity();
+   const Eigen::Matrix<std::complex<double>,3,3> cca = ckm * ckm.adjoint();
+   const double max_diff = (cca - unit).cwiseAbs().maxCoeff();
+
+   CHECK(max_diff < eps);
+}
