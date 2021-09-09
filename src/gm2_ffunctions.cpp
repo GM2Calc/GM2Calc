0cde987f005df2d7eeadb8aa46b15c3990f27d43
Alexander Voigt <ahgvoigt@gmail.com>
2021-09-09 14:49:01 +0200

    simplify expression for F3 from Eq. (28) arxiv:1502.04199
    for w < 1/4

--- src/gm2_ffunctions.cpp
+++ src/gm2_ffunctions.cpp
@@ -652,6 +652,17 @@ double F3(double w) noexcept {
    if (w < 0.0) {
       ERROR("F3: w must not be negative!");
       return std::numeric_limits<double>::quiet_NaN();
+   } else if (w < 0.25) {
+      const double zeta2 = 1.6449340668482264; // Zeta[2]
+      const double y = std::sqrt(1 - 4*w); // 0 < y < 1
+      const double l2 = 0.69314718055994531; // Log[2]
+      const double lw = std::log(w);
+      const double l1py = std::log(1 + y);
+      const double l1my = std::log(1 - y);
+      return 1 + 15*w + (1 + 15*w)*lw/2
+         + (-17 + 30*w)*w/y*(
+            + std::atanh(y)*(2*l2 + lw) + dilog((1 + y)/(-1 + y))
+            + zeta2/2 + (l1my - l1py)*(3*l1my + l1py)/4);
    } else if (w == 0.25) {
       return 19.0/4.0;
    }
