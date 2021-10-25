07792338c52142cff173bc214c4e0266aa2c2423
Alexander Voigt <alexander.voigt@hs-flensburg.com>
2021-10-25 15:16:33 +0200

    remove prefactor -i

--- src/THDM/gm2_2loop_B.cpp
+++ src/THDM/gm2_2loop_B.cpp
@@ -425,7 +425,7 @@ double amu2L_B_EWadd(const THDM_B_parameters& thdm) noexcept
    const double phi3 = 6*(-li3 + zeta2);
    const double phi4 = 6*(lh2/2 + 2*li4 + zeta2); // = (phi6 + 3*pi2 + 3*lh2)
    // 6*((sqr(log((1.0 - s0)/2.0)) - 2.0*dilog((1.0 - s0)/2.0))/s0)
-   const double phi5 = 6*(lc*std::atan2(-zw, 1.0) - 2*std::real(dilog((1.0 - s0)/2.0)*std::complex<double>(0.0,-1.0)))/zw;
+   const double phi5 = 6*(lc*std::atan2(-zw, 1.0) - 2*std::imag(dilog((1.0 - s0)/2.0)))/zw;
    const double phi6 = 6*(-li4 + zeta2);
    const double phi7 = 6*cw2*phi(xw);
 
