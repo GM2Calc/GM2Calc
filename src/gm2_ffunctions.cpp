e4ddd4a70c3f2b4747caa94f650ee1d7c5b6ef20
Alexander Voigt <ahgvoigt@gmail.com>
2022-01-27 14:19:22 +0100

    using Euler's identity to eliminate log(2) and log(1 - y)

--- src/gm2_ffunctions.cpp
+++ src/gm2_ffunctions.cpp
@@ -689,16 +689,13 @@ double F3(double w) noexcept {
       return std::numeric_limits<double>::quiet_NaN();
    } else if (w < 0.25) {
       const double zeta2 = 1.6449340668482264; // Zeta[2]
-      const double l2 = 0.69314718055994531; // Log[2]
       const double y = std::sqrt(1 - 4*w); // 0 < y < 1
       const double lw = std::log(w);
       const double q = (1 + y)/(1 - y);
       const double lq = std::log(q);
-      const double l1my = std::log(1 - y);
       return (1 + 15*w)*(1 + lw/2)
          + (-17 + 30*w)*w/y*(
-            + lq*(l2 + lw/2 - l1my - lq/4)
-            + dilog(-q) + zeta2/2);
+            lq*(lw/2 - lq/4) - dilog(1 + q) + 3*zeta2/2);
    } else if (w == 0.25) {
       return 19.0/4.0;
    }
