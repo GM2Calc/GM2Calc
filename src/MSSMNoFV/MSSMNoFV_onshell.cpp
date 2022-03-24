80bb314e5473f982ec511e4da9bc08a09bec6bdd
Alexander Voigt <ahgvoigt@gmail.com>
2022-03-24 20:46:32 +0100

    break also when old precision == new precision

--- src/MSSMNoFV/MSSMNoFV_onshell.cpp
+++ src/MSSMNoFV/MSSMNoFV_onshell.cpp
@@ -544,7 +544,7 @@ void MSSMNoFV_onshell::convert_Mu_M1_M2(
       const double old_precision = precision;
       precision = calc_precision(bino_idx_DR);
 
-      if (precision > old_precision) {
+      if (precision >= old_precision) {
          if (verbose_output) {
             VERBOSE("   No improvement in last iteration step, stopping iteration ...");
          }
@@ -824,7 +824,7 @@ double MSSMNoFV_onshell::convert_me2_fpi_modify(
       const double old_precision = precision;
       precision = calc_precision(right_index);
 
-      if (precision > old_precision) {
+      if (precision >= old_precision) {
          if (verbose_output) {
             VERBOSE("   No improvement in last iteration step, stopping iteration ...");
          }
