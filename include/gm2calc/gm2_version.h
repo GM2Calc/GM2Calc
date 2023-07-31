83d5fa8ba8b600b88b56e2938c991d157368e66b
Alexander Voigt <ahgvoigt@gmail.com>
2023-07-31 12:48:52 +0200

    version 2.2.0

--- include/gm2calc/gm2_version.h
+++ include/gm2calc/gm2_version.h
@@ -21,8 +21,8 @@
 
 /* GM2Calc version */
 #define GM2CALC_VERSION_MAJOR 2
-#define GM2CALC_VERSION_MINOR 1
+#define GM2CALC_VERSION_MINOR 2
 #define GM2CALC_VERSION_PATCH 0
-#define GM2CALC_VERSION "2.1.0"
+#define GM2CALC_VERSION "2.2.0"
 
 #endif
