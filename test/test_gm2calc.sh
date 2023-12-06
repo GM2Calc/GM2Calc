36de6388ab0cfae9cbe7b5f964dc4596709e8983
Alexander Voigt <ahgvoigt@gmail.com>
2023-12-06 07:42:33 +0100

    test all SLHA-like output formats

--- test/test_gm2calc.sh
+++ test/test_gm2calc.sh
@@ -42,7 +42,40 @@ test_detailed_output() {
     { cat $2
       cat <<EOF
 Block GM2CalcConfig
-     0     1     # minimal output
+     0     1     # detailed output
+EOF
+    } | ${GM2CALC} "--$1-input-file=-" >/dev/null 2>&1
+    expect_success "$?"
+}
+
+test_gm2calc_output() {
+    printf "%s" "test_gm2calc_output"
+    { cat $2
+      cat <<EOF
+Block GM2CalcConfig
+     0     4     # GM2CalcOutput
+EOF
+    } | ${GM2CALC} "--$1-input-file=-" >/dev/null 2>&1
+    expect_success "$?"
+}
+
+test_spheno_output() {
+    printf "%s" "test_spheno_output"
+    { cat $2
+      cat <<EOF
+Block GM2CalcConfig
+     0     3     # SPhenoLowEnergy
+EOF
+    } | ${GM2CALC} "--$1-input-file=-" >/dev/null 2>&1
+    expect_success "$?"
+}
+
+test_nmssmtools_output() {
+    printf "%s" "test_nmssmtools_output"
+    { cat $2
+      cat <<EOF
+Block GM2CalcConfig
+     0     2     # SPhenoLowEnergy
 EOF
     } | ${GM2CALC} "--$1-input-file=-" >/dev/null 2>&1
     expect_success "$?"
@@ -55,6 +88,15 @@ test_no_input_source "thdm"
 test_detailed_output "gm2calc" "${BASEDIR}/../input/example.gm2"
 test_detailed_output "slha" "${BASEDIR}/../input/example.slha"
 test_detailed_output "thdm" "${BASEDIR}/../input/example.thdm"
+test_gm2calc_output "gm2calc" "${BASEDIR}/../input/example.gm2"
+test_gm2calc_output "slha" "${BASEDIR}/../input/example.slha"
+test_gm2calc_output "thdm" "${BASEDIR}/../input/example.thdm"
+test_nmssmtools_output "gm2calc" "${BASEDIR}/../input/example.gm2"
+test_nmssmtools_output "slha" "${BASEDIR}/../input/example.slha"
+test_nmssmtools_output "thdm" "${BASEDIR}/../input/example.thdm"
+test_spheno_output "gm2calc" "${BASEDIR}/../input/example.gm2"
+test_spheno_output "slha" "${BASEDIR}/../input/example.slha"
+test_spheno_output "thdm" "${BASEDIR}/../input/example.thdm"
 
 count=$(expr $errors + $passes)
 
