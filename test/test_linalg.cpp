b51e27bbe17d8772f1c9bc64f8935211e6ee3d00
Alexander Voigt <ahgvoigt@gmail.com>
2023-12-11 22:34:52 +0100

    enable test case 1

--- test/test_linalg.cpp
+++ test/test_linalg.cpp
@@ -6,7 +6,6 @@
 #include <cmath>
 #include <complex>
 
-// #include <boost/test/unit_test.hpp>
 // #include <boost/mpl/pair.hpp>
 // #include <boost/mpl/list.hpp>
 // #include <boost/mpl/int.hpp>
@@ -15,10 +14,12 @@
 // #include <boost/mpl/push_front.hpp>
 // #include <boost/mpl/placeholders.hpp>
 
+
 #define CHECK_CLOSE(a,b,eps)                            \
    do {                                                 \
       CHECK((a) == doctest::Approx(b).epsilon(eps));    \
    } while (0)
+
 
 #define CHECK_SMALL(a,eps)                              \
    do {                                                 \
@@ -26,85 +27,88 @@
    } while (0)
 
 
-template<class S_, int M_, int N_,
-	 void fxn_(const Eigen::Matrix<S_, M_, N_>&,
-		   Eigen::Array<double, MIN_(M_, N_), 1>&,
-		   Eigen::Matrix<S_, M_, M_>&,
-		   Eigen::Matrix<S_, N_, N_>&),
-	 void svs_(const Eigen::Matrix<S_, M_, N_>&,
-		   Eigen::Array<double, MIN_(M_, N_), 1>&),
-	 bool check_ascending_order_ = false>
+template <class S_, int M_, int N_,
+          void fxn_(const Eigen::Matrix<S_, M_, N_>&,
+                    Eigen::Array<double, MIN_(M_, N_), 1>&,
+                    Eigen::Matrix<S_, M_, M_>&, Eigen::Matrix<S_, N_, N_>&),
+          void svs_(const Eigen::Matrix<S_, M_, N_>&,
+                    Eigen::Array<double, MIN_(M_, N_), 1>&),
+          bool check_ascending_order_ = false>
 struct Test_svd {
-    typedef S_ S;
-    enum { M = M_ };
-    enum { N = N_ };
-    enum { check_ascending_order = check_ascending_order_ };
-    void fxn(const Eigen::Matrix<S_, M_, N_>& m,
-	     Eigen::Array<double, MIN_(M_, N_), 1>& s,
-	     Eigen::Matrix<S_, M_, M_>& u,
-	     Eigen::Matrix<S_, N_, N_>& vh)
-    { fxn_(m, s, u, vh); }
-    void svs(const Eigen::Matrix<S_, M_, N_>& m,
-	     Eigen::Array<double, MIN_(M_, N_), 1>& s)
-    { svs_(m, s); }
+   typedef S_ S;
+   enum { M = M_ };
+   enum { N = N_ };
+   enum { check_ascending_order = check_ascending_order_ };
+   void fxn(const Eigen::Matrix<S_, M_, N_>& m,
+            Eigen::Array<double, MIN_(M_, N_), 1>& s,
+            Eigen::Matrix<S_, M_, M_>& u, Eigen::Matrix<S_, N_, N_>& vh)
+   {
+      fxn_(m, s, u, vh);
+   }
+   void svs(const Eigen::Matrix<S_, M_, N_>& m,
+            Eigen::Array<double, MIN_(M_, N_), 1>& s)
+   {
+      svs_(m, s);
+   }
 };
 
-#ifdef TEST_LINALG2_PART1
-typedef boost::mpl::list<
-    // use Eigen::JacobiSVD
-    Test_svd<std::complex<double>, 1, 1, svd, svd>,
-    Test_svd<std::complex<double>, 2, 2, svd, svd>,
-    Test_svd<std::complex<double>, 3, 3, svd, svd>,
-    Test_svd<std::complex<double>, 6, 6, svd, svd>,
-    Test_svd<double	    , 1, 1, svd, svd>,
-    Test_svd<double	    , 2, 2, svd, svd>,
-    Test_svd<double	    , 3, 3, svd, svd>,
-    Test_svd<double	    , 6, 6, svd, svd>,
 
-    Test_svd<std::complex<double>, 1, 1, reorder_svd, reorder_svd, true>,
-    Test_svd<std::complex<double>, 2, 2, reorder_svd, reorder_svd, true>,
-    Test_svd<std::complex<double>, 3, 3, reorder_svd, reorder_svd, true>,
-    Test_svd<std::complex<double>, 6, 6, reorder_svd, reorder_svd, true>,
-    Test_svd<std::complex<double>, 4, 6, reorder_svd, reorder_svd, true>,
-    Test_svd<std::complex<double>, 6, 4, reorder_svd, reorder_svd, true>,
-    Test_svd<double	    , 1, 1, reorder_svd, reorder_svd, true>,
-    Test_svd<double	    , 2, 2, reorder_svd, reorder_svd, true>,
-    Test_svd<double	    , 3, 3, reorder_svd, reorder_svd, true>,
-    Test_svd<double	    , 6, 6, reorder_svd, reorder_svd, true>,
-    Test_svd<double	    , 4, 6, reorder_svd, reorder_svd, true>,
-    Test_svd<double	    , 6, 4, reorder_svd, reorder_svd, true>
-> svd_tests;
-
-BOOST_AUTO_TEST_CASE_TEMPLATE(test_svd, T, svd_tests)
+TEST_CASE_TEMPLATE("test_svd", T,
+    Test_svd<std::complex<double>, 1, 1, gm2calc::svd, gm2calc::svd>,
+    Test_svd<std::complex<double>, 2, 2, gm2calc::svd, gm2calc::svd>,
+    Test_svd<std::complex<double>, 3, 3, gm2calc::svd, gm2calc::svd>,
+    Test_svd<std::complex<double>, 6, 6, gm2calc::svd, gm2calc::svd>,
+    Test_svd<double	         , 1, 1, gm2calc::svd, gm2calc::svd>,
+    Test_svd<double	         , 2, 2, gm2calc::svd, gm2calc::svd>,
+    Test_svd<double	         , 3, 3, gm2calc::svd, gm2calc::svd>,
+    Test_svd<double	         , 6, 6, gm2calc::svd, gm2calc::svd>,
+    Test_svd<std::complex<double>, 1, 1, gm2calc::reorder_svd, gm2calc::reorder_svd, true>,
+    Test_svd<std::complex<double>, 2, 2, gm2calc::reorder_svd, gm2calc::reorder_svd, true>,
+    Test_svd<std::complex<double>, 3, 3, gm2calc::reorder_svd, gm2calc::reorder_svd, true>,
+    Test_svd<std::complex<double>, 6, 6, gm2calc::reorder_svd, gm2calc::reorder_svd, true>,
+    Test_svd<std::complex<double>, 4, 6, gm2calc::reorder_svd, gm2calc::reorder_svd, true>,
+    Test_svd<std::complex<double>, 6, 4, gm2calc::reorder_svd, gm2calc::reorder_svd, true>,
+    Test_svd<double	         , 1, 1, gm2calc::reorder_svd, gm2calc::reorder_svd, true>,
+    Test_svd<double	         , 2, 2, gm2calc::reorder_svd, gm2calc::reorder_svd, true>,
+    Test_svd<double	         , 3, 3, gm2calc::reorder_svd, gm2calc::reorder_svd, true>,
+    Test_svd<double	         , 6, 6, gm2calc::reorder_svd, gm2calc::reorder_svd, true>,
+    Test_svd<double	         , 4, 6, gm2calc::reorder_svd, gm2calc::reorder_svd, true>,
+    Test_svd<double	         , 6, 4, gm2calc::reorder_svd, gm2calc::reorder_svd, true>
+   )
 {
-    typedef typename T::S S;
-    const Eigen::Index M = T::M;
-    const Eigen::Index N = T::N;
+   typedef typename T::S S;
+   const Eigen::Index M = T::M;
+   const Eigen::Index N = T::N;
 
-    Eigen::Matrix<S, M, N> m = Eigen::Matrix<S, M, N>::Random();
-    Eigen::Array<double, MIN_(M, N), 1> s;
-    Eigen::Matrix<S, M, M> u;
-    Eigen::Matrix<S, N, N> vh;
+   Eigen::Matrix<S, M, N> m = Eigen::Matrix<S, M, N>::Random();
+   Eigen::Array<double, MIN_(M, N), 1> s;
+   Eigen::Matrix<S, M, M> u;
+   Eigen::Matrix<S, N, N> vh;
 
-    T().fxn(m, s, u, vh);	// following LAPACK convention
-    Eigen::Matrix<S, M, N> sigma = u.adjoint() * m * vh.adjoint();
+   T().fxn(m, s, u, vh);	// following LAPACK convention
+   Eigen::Matrix<S, M, N> sigma = u.adjoint() * m * vh.adjoint();
 
-    BOOST_CHECK((s >= 0).all());
-    for (Eigen::Index i = 0; i < sigma.rows(); i++)
-	for (Eigen::Index j = 0; j < sigma.cols(); j++)
-	    BOOST_CHECK_SMALL(abs(sigma(i,j) - (i==j ? s(i) : 0)), 1e-13);
+   CHECK((s >= 0).all());
+   for (Eigen::Index i = 0; i < sigma.rows(); i++) {
+      for (Eigen::Index j = 0; j < sigma.cols(); j++) {
+         CHECK_SMALL(abs(sigma(i,j) - (i==j ? s(i) : 0)), 1e-13);
+      }
+   }
 
-    if (T::check_ascending_order)
-	for (Eigen::Index i = 0; i < s.size()-1; i++)
-	    BOOST_CHECK(s[i] <= s[i+1]);
+   if (T::check_ascending_order) {
+      for (Eigen::Index i = 0; i < s.size()-1; i++) {
+         CHECK(s[i] <= s[i+1]);
+      }
+   }
 
-    T().svs(m, s);
-    BOOST_CHECK((s >= 0).all());
-    for (Eigen::Index i = 0; i < sigma.rows(); i++)
-	for (Eigen::Index j = 0; j < sigma.cols(); j++)
-	    BOOST_CHECK_SMALL(abs(sigma(i,j) - (i==j ? s(i) : 0)), 1e-13);
+   T().svs(m, s);
+   CHECK((s >= 0).all());
+   for (Eigen::Index i = 0; i < sigma.rows(); i++) {
+      for (Eigen::Index j = 0; j < sigma.cols(); j++) {
+         CHECK_SMALL(abs(sigma(i,j) - (i==j ? s(i) : 0)), 1e-13);
+      }
+   }
 }
-#endif // TEST_LINALG2_PART1
 
 template<class S_, int N_,
 	 void fxn_(const Eigen::Matrix<S_, N_, N_>&,
