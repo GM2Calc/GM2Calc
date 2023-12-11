c517ad87de4ca80c4ced4d35ba15a97bfb7319d3
Alexander Voigt <alexander.voigt@hs-flensburg.com>
2023-12-11 08:27:36 +0100

    print u in debug output

--- test/test_linalg.cpp
+++ test/test_linalg.cpp
@@ -709,6 +709,7 @@ TEST_CASE("test_diagonalize_symmetric_errbd")
    Eigen::Array<double, 4, 1> u_errbd;
    gm2calc::fs_diagonalize_symmetric_errbd(m, s, &u, &s_errbd, &u_errbd);
    const double eps = std::sqrt(std::numeric_limits<double>::epsilon());
-   INFO(u*u.adjoint());
+   INFO("u =\n" << u);
+   INFO("uu^+ =\n" << (u*u.adjoint()).eval());
    CHECK(u.isUnitary(eps));
 }
