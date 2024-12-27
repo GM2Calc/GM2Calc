e694d79594c2fb52343dc8089fce04eecef7aed7
Alexander Voigt <alexander.voigt@physik.rwth-aachen.de>
2024-12-27 11:07:05 +0100

    adding tests for signed functions

--- test/test_numerics.cpp
+++ test/test_numerics.cpp
@@ -10,3 +10,17 @@ TEST_CASE("sign")
    CHECK_EQ(gm2calc::sign(-2), -1.0);
    CHECK_EQ(gm2calc::sign(0), 1.0);
 }
+
+
+TEST_CASE("signed_sqr")
+{
+   CHECK_EQ(gm2calc::signed_sqr(2), 4.0);
+   CHECK_EQ(gm2calc::signed_sqr(-2), -4.0);
+}
+
+
+TEST_CASE("signed_abs_sqrt")
+{
+   CHECK_EQ(gm2calc::signed_abs_sqrt(4), 2.0);
+   CHECK_EQ(gm2calc::signed_abs_sqrt(-4), -2.0);
+}
